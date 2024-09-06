0aa0e6699d81cef5fb4521a8df5f1cebb403475e
ggarber42 <ggarber@protonmail.com>
2024-09-05 23:23:20 -0300

    feat: add go routines to produce

--- internal/infra/output/kafka_producer_controller.go
+++ internal/infra/output/kafka_producer_controller.go
@@ -7,16 +7,33 @@ import (
 	kafka_producer "kafkago/internal/kafka/producer"
 )
 
-func KafkaProducerController(kProducer *kafka_producer.KafkaProducer, cfg *configs.Config, logger logger.ILogger) {
-	count := 1
+const (
+	NUM_WORKERS = 100
+)
+
+func worker(cfg *configs.Config, kProducer *kafka_producer.KafkaProducer, logger logger.ILogger, countChan <-chan int) {
 	for {
-		key := fmt.Sprintf("%s:%d", "chave", count)
+		key := fmt.Sprintf("%s:%d", "chave", <-countChan)
 		err := kProducer.Produce(cfg.Topic_1, "messagem", key)
 		if err != nil {
 			logger.Error(fmt.Sprintf("erro writing to kafka topic: %w", err))
 		} else {
-			logger.Info(fmt.Sprintf("produced message %d", count))
+			logger.Info(fmt.Sprintf("produced message %d", <-countChan))
 		}
+	}
+}
+
+func KafkaProducerController(kProducer *kafka_producer.KafkaProducer, cfg *configs.Config, logger logger.ILogger) {
+	countChan := make(chan int, NUM_WORKERS)
+
+	for i := 0; i < NUM_WORKERS; i++ {
+		go worker(cfg, kProducer, logger, countChan)
+	}
+
+	count := 1
+	for {
+		countChan <- count
 		count ++
 	}
+
 }
